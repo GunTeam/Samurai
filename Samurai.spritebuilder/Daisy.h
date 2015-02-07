9538b695977e26842435417bcf3eefd11dfe42e0
Laura Breiman <lauracle@mit.edu>
2015-02-06 20:36:56 -0500

    flower skeletons

---
+++ Samurai.spritebuilder/Daisy.h
@@ -0,0 +1,17 @@
+//
+//  Daisy.h
+//  Samurai
+//
+//  Created by Laura Breiman on 2/6/15.
+//  Copyright 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+#import "Flower.h"
+
+@interface Daisy : Flower {
+    
+}
+
+@end
---
+++ Samurai.spritebuilder/Daisy.m
@@ -0,0 +1,14 @@
+//
+//  Daisy.m
+//  Samurai
+//
+//  Created by Laura Breiman on 2/6/15.
+//  Copyright 2015 Apportable. All rights reserved.
+//
+
+#import "Daisy.h"
+
+
+@implementation Daisy
+
+@end
